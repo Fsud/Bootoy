288f748c8dc3c6ded5c1e93347734a037ebfeda5
Fsud <gfanle111@gmail.com>
2018-07-07 21:24:44 +0800

    first commit

---
+++ src/main/java/io/github/sudden/Bootoy/BootoyApplication.java
@@ -0,0 +1,12 @@
+package io.github.sudden.Bootoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootoyApplication.class, args);
+	}
+}
